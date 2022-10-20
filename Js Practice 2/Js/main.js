368de378d95e1df6df050e317a3e6826c9c9edd5
prabakar1506 <ranjithprabakar78@gmail.com>
2022-10-20 08:21:53 +0530

    add

---
+++ Js Practice 2/Js/main.js
@@ -0,0 +1,201 @@
+// Asignment Operators  ( = ,+= , -= , *= , %= , * * , /=)
+
+
+//Equal To Operator - =
+
+let a="Hello";
+    
+let b=a
+	
+console.log(b);	
+
+console.log(typeof(b));
+
+// Addition Operator - +=
+
+var x=10;
+    
+	x+=1;
+
+console.log(x);
+
+    x+=2;
+
+console.log(x);
+
+// Subtraction Operator - -=
+
+let y=12;
+  
+    y-=2;
+	
+console.log(y);
+
+console.log(typeof(y));
+
+
+// Multiplication Operator - *=
+
+let z=12;
+
+    z*=2;
+	 
+console.log(z);
+
+console.log(typeof(z));
+
+
+//Division Operator 	 - /=
+
+var p=15;
+    
+    p/=5;	
+
+console.log("Division");	
+	
+console.log(p);
+
+// Expotential Operator - **=
+
+let r=15;
+
+    r**=3;
+
+console.log("Expotential Operator");
+	
+console.log(r);
+
+// Percentage Operator   -  %=
+
+let e=10;
+
+    e%=5;
+
+console.log("Remainder Assignment");
+	
+console.log(e);
+
+
+// Arthematic Operators ( + , - , * , ** , / , % , ++ ,-- )
+
+
+// Addition Operator - +
+
+let num1=15, num2=5;
+
+console.log(num1+num2);
+
+console.log("Addition");
+
+sum=num1+num2;
+
+console.log(sum);
+
+
+// Subtraction Operator - -
+
+console.log(num1-num2);
+
+console.log("Subtraction");
+
+sum=num1-num2;
+
+console.log(sum);
+
+// Multiplication Operator - *
+
+console.log("Multiplication");
+
+console.log(num1*num2);
+
+// Division Operator - /
+
+console.log("Division");
+
+console.log(num1/num2);
+
+// Exponentiation Operator - **
+
+console.log("Exponentiation");
+
+console.log(num1/num2);
+
+// Modulus Or Remainder Operator - %
+
+console.log("Modulus");
+
+console.log(num1%num2);
+
+//  Increment Operator - ++
+
+let d=12;
+
+console.log(++d);
+
+console.log(" Pre Increment");
+
+console.log(d);
+
+//  Post Increment Operator - f++
+
+let f=5;
+
+console.log(f++);
+
+console.log(" Post Increment");
+
+console.log(f);
+
+//  Pre Decrement Operator - --g
+
+let g=11;
+
+console.log(--g);
+
+console.log(" Pre decrement");
+
+console.log(g);
+
+//  Post Increment Operator - h--
+
+let h=5;
+
+console.log(h--);
+
+console.log(" Post decrement");
+
+console.log(h);
+
+
+// Comparison Operator 
+
+
+//less than operator <
+
+let j=2 , k=3;
+ 
+ console.log(j<k);
+ 
+ //Greater than operator <
+
+let l=20 , v=13;
+ 
+ console.log(l>v);
+ 
+  //Greater than or equal to operator <
+
+let t=21 , u=20;
+ 
+ console.log(t>=u);
+
+ 
+  //less than or equal to operator <
+
+let s=19 , m=20;
+ 
+ console.log(s>=m);
+ 
+ co
+
+
+ 
