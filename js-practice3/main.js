e79cd6172150e858a34e4f478e469e141bf62ec2
prabakar1506 <ranjithprabakar78@gmail.com>
2022-11-01 22:20:15 +0530

    add

---
+++ js-practice3/main.js
@@ -0,0 +1,37 @@
+// Find Addition
+// Two Variables X and Y
+// Two values  10 and 30
+// Addition Operator (+)
+
+let x=10, y=30;
+
+console.log(x+y);
+
+sum=x+y;
+
+console.log("Addition Operator")
+
+console.log(sum);
+
+// Find Subtraction
+// Two Variables X and Y
+// Two Values 10 and 30
+// Subtraction Operator (-)
+
+let a=5, b=2;
+
+console.log(a-b);
+
+sum=a-b;
+
+console.log("subtraction");
+
+console.log(sum);
+
+let q=3;
+
+let w=5;
+
+sum=q+w;
+
+console.log(sum);
