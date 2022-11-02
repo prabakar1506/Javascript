3b3b19102e1b30d0bcca16b5fad0951dee4ca7e8
prabakar1506 <ranjithprabakar78@gmail.com>
2022-11-02 14:40:49 +0530

    add

--- js-practice3/main.js
+++ js-practice3/main.js
@@ -28,10 +28,61 @@ console.log("subtraction");
 
 console.log(sum);
 
+// Find Multiplication
+// Two Variables X and Y
+// Two Values 10 and 30
+// Subtraction Operator (*)
+
 let q=3;
 
 let w=5;
 
-sum=q+w;
+sum=q*w;
 
+console.log("multiplication");
 console.log(sum);
+
+
+
+
+
+// Nested Loop
+
+let i,j;
+
+for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+	
+	document.write(i);
+    }
+    document.write("<br>");
+    }
+
+
+let A,B;
+
+for(A=5;A>=1;A--){
+	for(B=1;B<=A;B++){
+	document.write(B);
+	}
+    document.write("<br>");
+}
+ 
+let C,D;
+
+for(C=65;C<=69;C++){
+	for(D=65;D<=C;D++){
+		document.write(String.fromCharCode(D));
+	}
+         document.write("<br>");
+    }
+	
+for(C=0;C<=9;C++){
+	for(D=0;D<=C;D++){
+		document.write(D);
+		
+	}
+         document.write("<br>");
+    }	
+
+	
