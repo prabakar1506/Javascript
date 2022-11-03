973e2f2a76a3d0d1a5c2a8d277b0fb1043058298
prabakar1506 <ranjithprabakar78@gmail.com>
2022-11-03 08:30:46 +0530

    add

--- Js Programs/main.js
+++ Js Programs/main.js
@@ -1,1 +1,106 @@
+//Program To Check Number Is Positive Or Negative
 
+   let x = prompt("enter a number");
+		
+   if(x>-1){
+           document.write("number is positive");
+           }
+		   else{
+                document.write("number is negative");
+           }
+		   {
+	        document.write("<br>");
+			document.write("<br>");
+	       }
+		   
+//Program To Check Number Is Greater Than 10
+
+   if(x>10){
+	        document.write("number is greater than 10");
+   }
+   else{
+	    document.write("number is less than 10");
+   }
+   {
+	document.write("<br>");
+	document.write("<br>");
+   }
+   
+   
+//number is even or odd
+
+  if(x%2==0){
+	        document.write("number is Even");
+   }
+   else{
+	    document.write("number is Odd");
+   }
+    {
+	document.write("<br>");
+	document.write("<br>");
+   }
+   
+//Program to check whether number is divisible by 5
+
+     if(x%5==0){
+	        document.write("number is divisible by 5");
+   }
+   else{
+	    document.write("number is not divisible by 5");
+   }
+    {
+	document.write("<br>");
+	document.write("<br>");
+   }
+
+//to check whether number is multiple of 7
+
+  
+   if(x%7==0){
+	        document.write("number is multiplication of 7");
+   }
+   else{
+	    document.write("number is not multiplication of 7");
+   }
+    {
+	document.write("<br>");
+	document.write("<br>");
+   }
+   
+//program to find marriage eligiblity
+
+   if(x>=21){
+	        document.write("Eligible For Marriage");
+   }
+   else{
+	    document.write("Not Eligible For Marriage");
+   }
+    {
+	document.write("<br>");
+	document.write("<br>");
+   }
+
+   
+//print grade based on marks of a student
+
+   if(x<60){
+	        document.write("Grade E");
+   }
+   else if(x<70){
+	    document.write("Grade D");
+   }
+    else if(x<80){
+	    document.write("Grade C");
+   }
+    else if(x<100){
+	    document.write("Grade B");
+   }
+    else if(x<100){
+	    document.write("Grade A");
+   }
+    {
+	document.write("<br>");
+	document.write("<br>");
+   }
+  
+		
