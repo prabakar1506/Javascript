a9ba92c06e614be8c10d9a486eba9d26b4c4b4b1
prabakar1506 <ranjithprabakar78@gmail.com>
2022-11-07 19:14:36 +0530

    add

--- Js Program-2/main.js
+++ Js Program-2/main.js
@@ -15,4 +15,33 @@ function reverseNum(num) {
     ) * Math.sign(num)
   )
 }
-prompt(reverseNum(123456));
+document.write("The Reversed Number Is ");
+document.write(reverseNum(123456));
+document.write("<br><br>");
+
+
+
+
+
+//Calculate sum of a number
+
+const num1 = parseInt(prompt('Enter the first number'));
+const num2 = parseInt(prompt('Enter the second number'));
+
+const num=num1+num2;
+
+document.write("sum of a number  ");
+document.write(num);
+document.write("<br><br>");
+
+
+//print table of number
+
+const num3 = parseInt(prompt('Enter the first number'));
+const num4 = parseInt(prompt('Enter the second number'));
+
+const table=num3*num4;
+
+document.write("table of number ");
+document.write(table);
+document.write("<br><br>");
