1a7470be619ff04213be8bbd06c1244dc9e70e38
prabakar1506 <ranjithprabakar78@gmail.com>
2022-11-29 17:02:56 +0530

    add

--- array object/main.js
+++ array object/main.js
@@ -1,19 +1,19 @@
 // Array Object
 
-let length=parseInt(prompt("Enter The Array Length"))
-let object=[]
+// let length=parseInt(prompt("Enter The Array Length"))
+// let object=[]
 
-let Person
+// let Person
 
-for(i=0;i<length;i++){
-	person={name:"",rollNo:"",Department:""}
-	person.name=prompt("Enter the Name of The Student")
-	person.rollNo=parseInt(prompt("Enter The Rollno Of The Student"))
-	person.Department=prompt("Enter The Department Of The Student")
+// for(i=0;i<length;i++){
+	// person={name:"",rollNo:"",Department:""}
+	// person.name=prompt("Enter the Name of The Student")
+	// person.rollNo=parseInt(prompt("Enter The Rollno Of The Student"))
+	// person.Department=prompt("Enter The Department Of The Student")
 	
-	object[i]=person
-}
-  console.log(object);
+	// object[i]=person
+// }
+  // console.log(object);
   
   
 // Array 
@@ -29,3 +29,43 @@ for(i=0;i<length;i++){
        // array[i]=persons
    // }
      // console.log(array)
+	 
+function pel(z){
+            let b=[];
+            for(i=0;i<z;i++){
+                d=prompt("enter inner length");
+                if(d==0){
+                    d=parseInt(prompt("enter the number"))
+                }
+                else{
+                    c=[];
+                    for(j=0;j<d;j++){
+                        var e=prompt("enter the value");
+                        c[j]=e;
+                    }
+                }
+				b[i]=c;
+            }
+            return b;
+        }
+
+        function go(no1,no2){
+            if (x<g.length){
+                var vv=(g[no1][no2]);
+            }
+            else{
+                console.log("not this value")
+            }
+            return vv;
+        }
+
+        let a=prompt("enter the main index");
+        let g=pel(a);
+
+        console.log(g);
+
+        let x=prompt("enter the big index");
+        let y=prompt("enter the index");
+        let s= go(x,y);
+       
+        console.log(s);
